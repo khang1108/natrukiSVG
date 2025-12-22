583415b9a93aa0b7b684f34bb06c487454f5b3e6
pumpomeo <thihoang.31241027692@st.ueh.edu.vn>
2025-12-22 14:41:32 +0700

    click + cursor

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,24 +1,35 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QCursor>
 #include <QLocale>
+#include <QPixmap>
 #include <QTranslator>
 
 int main(int argc, char* argv[])
 {
-  QApplication a(argc, argv);
-  a.setWindowIcon(QIcon(":/resources/images/logo.png"));  
+    QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/resources/images/logo.png"));
 
-  QTranslator translator;
-  const QStringList uiLanguages = QLocale::system().uiLanguages();
-  for (const QString& locale : uiLanguages) {
-    const QString baseName = "NaTrukiSVG_" + QLocale(locale).name();
-    if (translator.load(":/i18n/" + baseName)) {
-      a.installTranslator(&translator);
-      break;
+    // Set custom cursor
+    QPixmap cursorPixmap(":/icons/resources/icons/cursor.png");
+    if (!cursorPixmap.isNull()) {
+        // Scale cursor to appropriate size (32x32 is standard)
+        cursorPixmap = cursorPixmap.scaled(32, 32, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        QCursor customCursor(cursorPixmap);
+        a.setOverrideCursor(customCursor);
     }
-  }
-  MainWindow w;
-  w.show();
-  return a.exec();
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString& locale : uiLanguages) {
+        const QString baseName = "NaTrukiSVG_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
