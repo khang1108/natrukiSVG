6afdf7ac1fcaf73188a589dc2aee9a5a24d19d35
Nguyen Phuc Khang <nguyenphuc.khang110806@gmail.com>
2025-12-22 08:37:31 +0700

    update report v2

--- src/svg/SVGStyle.cpp
+++ src/svg/SVGStyle.cpp
@@ -7,18 +7,18 @@ namespace
 
     bool IsColorSet(const SVGColor& color)
     {
-        if (color.isNone) {
-            return true;
-        }
+		if (color.isNone) {
+			return true;
+		}
 
-        return color.a > 0;
-    }
+		return color.a > 0;
+	}
 
-    const SVGNumber UNSET_NUMBER = -1.0;
+	const SVGNumber UNSET_NUMBER = -1.0;
 } // namespace
 
 SVGStyle::SVGStyle()
-  : fillColor{0, 0, 0, 255}, strokeColor{0, 0, 0, 0, true}, strokeWidth(1.0), fillOpacity(1.0),
+    : fillColor{0, 0, 0, 255}, strokeColor{0, 0, 0, 0, true}, strokeWidth(1.0), fillOpacity(1.0),
     strokeOpacity(1.0), fillRule(SVGFillRule::NonZero), fontSize(16.0)
 {
 
@@ -26,15 +26,15 @@ SVGStyle::SVGStyle()
 
     strokeColor = {0, 0, 0, 0, false};
 
-    strokeWidth = UNSET_NUMBER;
+	strokeWidth = UNSET_NUMBER;
 
-    fillOpacity = UNSET_NUMBER;
+	fillOpacity = UNSET_NUMBER;
 
-    strokeOpacity = UNSET_NUMBER;
+	strokeOpacity = UNSET_NUMBER;
 
-    fillRule = SVGFillRule::NonZero;
+	fillRule = SVGFillRule::NonZero;
 
-    fontSize = UNSET_NUMBER;
+	fontSize = UNSET_NUMBER;
 }
 
 /**
@@ -60,46 +60,46 @@ void SVGStyle::inheritFrom(const SVGStyle& parent)
 {
 
     // Inherit fill color if not set
-    if (!IsColorSet(this->fillColor)) {
-        this->fillColor = parent.fillColor;
-    }
+	if (!IsColorSet(this->fillColor)) {
+		this->fillColor = parent.fillColor;
+	}
 
     // Inherit stroke color if not set
-    if (!IsColorSet(this->strokeColor)) {
-        this->strokeColor = parent.strokeColor;
-    }
+	if (!IsColorSet(this->strokeColor)) {
+		this->strokeColor = parent.strokeColor;
+	}
 
     // Inherit stroke width if not set
-    if (this->strokeWidth == UNSET_NUMBER) {
-        this->strokeWidth = parent.strokeWidth;
-    }
+	if (this->strokeWidth == UNSET_NUMBER) {
+		this->strokeWidth = parent.strokeWidth;
+	}
 
     // Inherit fill opacity if not set
-    if (this->fillOpacity == UNSET_NUMBER) {
-        this->fillOpacity = parent.fillOpacity;
-    }
+	if (this->fillOpacity == UNSET_NUMBER) {
+		this->fillOpacity = parent.fillOpacity;
+	}
 
     // Inherit stroke opacity if not set
-    if (this->strokeOpacity == UNSET_NUMBER) {
-        this->strokeOpacity = parent.strokeOpacity;
-    }
+	if (this->strokeOpacity == UNSET_NUMBER) {
+		this->strokeOpacity = parent.strokeOpacity;
+	}
 
     // Inherit fill rule (only EvenOdd from parent if child doesn't have it)
-    if (this->fillRule != SVGFillRule::EvenOdd) {
-        if (parent.fillRule == SVGFillRule::EvenOdd) {
-            this->fillRule = SVGFillRule::EvenOdd;
-        }
-    }
+	if (this->fillRule != SVGFillRule::EvenOdd) {
+		if (parent.fillRule == SVGFillRule::EvenOdd) {
+			this->fillRule = SVGFillRule::EvenOdd;
+		}
+	}
 
     // Inherit font family if not set
-    if (this->fontFamily.empty()) {
-        this->fontFamily = parent.fontFamily;
-    }
+	if (this->fontFamily.empty()) {
+		this->fontFamily = parent.fontFamily;
+	}
 
     // Inherit font size if not set
-    if (this->fontSize == UNSET_NUMBER) {
-        this->fontSize = parent.fontSize;
-    }
+	if (this->fontSize == UNSET_NUMBER) {
+		this->fontSize = parent.fontSize;
+	}
 
     // If parent is not displayed, child is also not displayed
     if (!parent.isDisplayed) {
